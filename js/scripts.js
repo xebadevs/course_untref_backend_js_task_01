a0d785d091050277b25f1484a4c6398affe021e1
Sebastián Londero <xebadevs@gmail.com>
2023-06-28 12:31:51 +1000

    setGuitar method

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,21 @@ function getAllGuitars() {
             console.error('Error', error);
         });
 }
+
+function setGuitar(guitarData) {
+
+    fetch('http://localhost:3000/guitars', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(guitarData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error', error);
+        });
+}
